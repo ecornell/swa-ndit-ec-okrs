78187d259165334176169a2f84b05b5a3f32aba6
Eli <ecornell@nd.gov>
2022-03-17 14:12:27 -0500

    add app insight

--- okrs/src/main.js
+++ okrs/src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import { createPinia, PiniaPlugin } from 'pinia'
+import {
+  createPinia,
+  PiniaPlugin
+} from 'pinia'
 import VueCompositionAPI from '@vue/composition-api'
+import {
+  ApplicationInsights
+} from '@microsoft/applicationinsights-web'
+
+let insightKey = process.env.VUE_APP_APPINSIGHTS_INSTRUMENTATIONKEY;
+if (insightKey) {
+  const appInsights = new ApplicationInsights({
+    config: {
+      instrumentationKey: process.env.VUE_APP_APP_INSIGHT_KEY
+    }
+  });
+  appInsights.loadAppInsights();
+  appInsights.trackPageView();
+}
 
 Vue.use(VueCompositionAPI)
 Vue.use(PiniaPlugin)
